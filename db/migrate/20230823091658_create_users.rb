7277adc5d1a9757a3184f9f74989d8a1203c3e35
tgi-wtag <timo.giger@welldev.io>
2023-08-23 13:44:19 +0200

    Update 20230823091658_create_users.rb

--- db/migrate/20230823091658_create_users.rb
+++ db/migrate/20230823091658_create_users.rb
@@ -1,18 +1,18 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
 
-def change
-  create_table :users, id: false do |t|
-    t.integer "user_id", primary_key: true #primary key
-    t.string "fist_name", :limit => 25
-    t.string "last_name", :limit => 25
-    t.string "role", :default => 'user'
-    t.date "date_of_birth"
-    t.date "joining_date"
-    t.timestamps
-    #t.datetime "created_at"
-    #t.datetime "updated_at"
-  end
+  def change
+    create_table :users, id: false do |t|
+      t.integer "user_id", primary_key: true #primary key
+      t.string "fist_name", :limit => 25
+      t.string "last_name", :limit => 25
+      t.string "role", :default => 'user'
+      t.date "date_of_birth"
+      t.date "joining_date"
+      t.timestamps
+      #t.datetime "created_at"
+      #t.datetime "updated_at"
+    end
 
-end
+  end
 
 end
