60a2d3361d0b2db844e88b6ef0c8a4f71558382d
tgi-wtag <timo.giger@welldev.io>
2023-08-28 09:52:34 +0200

    created veiws

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/pages/home', to: 'pages#home'
   get '/log/entries', to: 'log#entries'
+  get 'tasks', to: 'pages#tasks', as: 'tasks'
+  get 'goals', to: 'pages#goals', as: 'goals'
+  get 'profile', to: 'pages#profile', as: 'profile'
 end
 
