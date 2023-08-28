41439451cd9595dd65ad2991e48480378584d3a1
tgi-wtag <timo.giger@welldev.io>
2023-08-28 13:08:35 +0200

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
     def home
     end
+    
+    def create_entry
+      @entry = JournalEntry.new
+      # You might also want to initialize any associated objects here (e.g., tasks, goals)
+    end
   end
