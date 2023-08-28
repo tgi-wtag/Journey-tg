67fb1f30defc1bb34e705c5691482a7a7fc67252
tgi-wtag <timo.giger@welldev.io>
2023-08-28 14:07:49 +0200

    Update 20230823091659_create_journal_entries.rb

--- db/migrate/20230823091659_create_journal_entries.rb
+++ db/migrate/20230823091659_create_journal_entries.rb
@@ -1,11 +1,12 @@
 class CreateJournalEntries < ActiveRecord::Migration[7.0]
   def change
     create_table :journal_entries do |t|
-      t.references :user, foreign_key: true
       t.string :title
+      t.integer :user_id
       t.date :date
       t.text :content
       t.timestamps
     end
+    
   end
 end
