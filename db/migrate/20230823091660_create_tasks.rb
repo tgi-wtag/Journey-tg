a1a4388e29165cf7a110a71824771d8db3e98084
tgi-wtag <timo.giger@welldev.io>
2023-08-28 14:09:11 +0200

    .

--- db/migrate/20230823091660_create_tasks.rb
+++ db/migrate/20230823091660_create_tasks.rb
@@ -1,8 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.references :user, null: false, foreign_key: true
       t.string :title
+      t.integer :user_id
       t.text :description
       t.string :status
       t.string :attached_document
--- db/migrate/20230823091700_create_goals.rb
+++ db/migrate/20230823091700_create_goals.rb
@@ -1,8 +1,8 @@
 class CreateGoals < ActiveRecord::Migration[7.0]
   def change
     create_table :goals do |t|
-      t.references :user, null: false, foreign_key: true
       t.text :description
+      t.integer :user_id
       t.date :deadline
 
       t.timestamps
