6414a1278ed9099382c1a14cbd0e3da230252e2e
tgi-wtag <timo.giger@welldev.io>
2023-08-28 12:58:57 +0200

    error fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'create_entry', to: 'pages#create_entry' as: 'create_entry'
+  get 'create_entry', to: 'pages#create_entry', as: 'create_entry'
   get 'entries', to: 'pages#entries', as: 'entries'
   get 'tasks', to: 'pages#tasks', as: 'tasks'
   get 'goals', to: 'pages#goals', as: 'goals'
