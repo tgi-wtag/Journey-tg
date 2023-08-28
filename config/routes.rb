2250b2ee4dac6a139e7390685a6e431fb710c900
tgi-wtag <timo.giger@welldev.io>
2023-08-28 10:55:43 +0200

    chahnged routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get '/pages/home', to: 'pages#home'
-  get '/log/entries', to: 'log#entries'
+  get 'entries', to: 'pages#entries', as: 'entries'
   get 'tasks', to: 'pages#tasks', as: 'tasks'
   get 'goals', to: 'pages#goals', as: 'goals'
   get 'profile', to: 'pages#profile', as: 'profile'
