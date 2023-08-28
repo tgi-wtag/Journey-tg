0fee963dfdad9db2c40c9c769faebe519ade4136
tgi-wtag <timo.giger@welldev.io>
2023-08-28 15:40:35 +0200

    deleted unececary things

--- db/migrate/20230823091658_create_users.rb
+++ db/migrate/20230823091658_create_users.rb
@@ -2,15 +2,13 @@ class CreateUsers < ActiveRecord::Migration[7.0]
 
   def up
     create_table :users, id: false do |t|
-      t.integer "user_id", primary_key: true #primary key
+      t.integer "user_id", primary_key: true
       t.string "fist_name", :limit => 25
       t.string "last_name", :limit => 25
       t.string "role", :default => 'user'
       t.date "date_of_birth"
       t.date "joining_date"
       t.timestamps
-      #t.datetime "created_at"
-      #t.datetime "updated_at"
     end
 
   end
--- test/models/create_journal_entry_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CreateJournalEntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
