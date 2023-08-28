71cf7cde3c9b29f6bdcc0d48ab464330f5766b4a
tgi-wtag <timo.giger@welldev.io>
2023-08-28 13:44:15 +0200

    .

--- db/migrate/20230823091658_create_users.rb
+++ db/migrate/20230823091658_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
 
-  def change
+  def up
     create_table :users, id: false do |t|
       t.integer "user_id", primary_key: true #primary key
       t.string "fist_name", :limit => 25
@@ -13,6 +13,10 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       #t.datetime "updated_at"
     end
 
+  end
+
+  def down 
+    drop_table : users;
   end
 
 end
