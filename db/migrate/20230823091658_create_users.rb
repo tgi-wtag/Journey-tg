9d3ed11523540f0a5fad7a4620f764926d46b43f
tgi-wtag <timo.giger@welldev.io>
2023-08-29 13:56:52 +0200

    deleted limit

--- db/migrate/20230823091658_create_users.rb
+++ db/migrate/20230823091658_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
 
   def up
     create_table :users do |t|
-      t.string "fist_name", :limit => 25
-      t.string "last_name", :limit => 25
+      t.string "fist_name" 
+      t.string "last_name"
       t.string "role", :default => 'user'
       t.date "date_of_birth"
       t.date "joining_date"
