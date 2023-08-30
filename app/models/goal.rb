0128273baa98481e4a2220611f430d9d3465d338
tgi-wtag <timo.giger@welldev.io>
2023-08-30 13:59:59 +0200

    enum status

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,7 @@
 class Goal < ApplicationRecord
   # belongs_to :user
+  enum status: {
+    setted: 0,
+    workingon: 1,
+    archived: 2
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
   # belongs_to :user
+  enum status: {
+    todo: 0,
+    inprogress: 1,
+    finished: 2
 end
