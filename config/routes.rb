55238fd1705ad815553793370472f629aa622aae
tgi-wtag <timo.giger@welldev.io>
2023-08-31 09:40:42 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'new_entry', to: 'pages#new_entry', as: 'new_entry'
-  post 'create_entry', to: 'pages#create_entry'
   get 'entries', to: 'pages#entries', as: 'entries'
   get 'tasks', to: 'pages#tasks', as: 'tasks'
   get 'goals', to: 'pages#goals', as: 'goals'
   get 'profile', to: 'pages#profile', as: 'profile'
+
+  get 'create_entry', to: 'pages#new_entry', as: 'new_entry'
+  post 'create_entry', to: 'pages#create_entry', as: 'create_entry'
 end
