9d1ddd0788251ee4fc10d68011126e5f0b4922ec
tgi-wtag <timo.giger@welldev.io>
2023-08-31 10:07:01 +0200

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,13 +7,13 @@ class PagesController < ApplicationController
 
     # Handle the entry creation here if needed
 
-    # Example:
-    if @entry.save
-      redirect_to some_path
-    else
-      render :new_entry_form
-    end
-  end
+  #   # Example:
+  #   if @entry.save
+  #     redirect_to some_path
+  #   else
+  #     render :new_entry_form
+  #   end
+  # end
 
   def new_entry_form
     @entry = JournalEntry.new
