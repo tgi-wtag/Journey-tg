cabc92888a6dc72304da3455f0ff8e2845011906
tgi-wtag <timo.giger@welldev.io>
2023-08-31 10:04:00 +0200

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,18 @@ class PagesController < ApplicationController
   def create_entry
     @entry = JournalEntry.new(entry_params)
 
-    # if @entry.save
-    #   redirect_to some_path
-    # else
-    #   render :new
-    # end
+    # Handle the entry creation here if needed
+
+    # Example:
+    if @entry.save
+      redirect_to some_path
+    else
+      render :new_entry_form
+    end
+  end
+
+  def new_entry_form
+    @entry = JournalEntry.new
   end
 
   private
@@ -18,10 +25,3 @@ class PagesController < ApplicationController
     params.require(:journal_entry).permit(:title, :date, :content)
   end
 end
-
-def new_entry_form
-  @entry = JournalEntry.new
-end
-
-# def create_entry
-# end
