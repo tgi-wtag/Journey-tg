6f2dc98a78f11481e992b193eaaa4c649dd33a36
tgi-wtag <timo.giger@welldev.io>
2023-09-06 11:54:51 +0200

    changed routing system

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,14 +10,22 @@ class PagesController < ApplicationController
     @entry = JournalEntry.new
   end
 
+  def entries;end
+
+  def goals;end
+
+  def profile;end
+
   def create_entry
     @entry = JournalEntry.new(entry_params)
     if @entry.save
-      redirect_to entries_path
+      redirect_to entries_pages_path
     else
       render :new_entry_form
     end
   end
+
+  def tasks;end
 
   private
   def entry_params
