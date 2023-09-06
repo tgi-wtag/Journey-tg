4a1e46af41c398dfa94166cb4633dd7ed2fe822e
tgi-wtag <timo.giger@welldev.io>
2023-09-06 08:56:16 +0200

    new routing style

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'entries', to: 'pages#entries', as: 'entries'
-  get 'tasks', to: 'pages#tasks', as: 'tasks'
-  get 'goals', to: 'pages#goals', as: 'goals'
-  get 'profile', to: 'pages#profile', as: 'profile'
 
-  get 'new_entry_form', to: 'pages#new_entry_form', as: 'new_entry_form'
-  post 'create_entry', to: 'pages#create_entry', as: 'create_entry'
+  resources :pages do
+    collection do
+      get 'entries'
+      get 'tasks'
+      get 'goals'
+      get 'profile'
+      get 'new_entry_form', to: 'pages#new_entry_form'
+      post 'create_entry', to: 'pages#create_entry'
+    end
+  end
 end
