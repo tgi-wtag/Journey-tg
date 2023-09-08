79f2e265eff62c14c5c118f0d4a7cbf9c133e7cf
tgi-wtag <timo.giger@welldev.io>
2023-09-08 11:36:06 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-
-
   resources :journal_entries, only: [:index, :new, :create]
-
   resources :tasks, only: [:index]
-
   resources :goals, only: [:index]
-
   get 'profile', to: 'profile#profile', as: 'profile'
   get 'home', to: 'pages#home', as: 'home'
 end
