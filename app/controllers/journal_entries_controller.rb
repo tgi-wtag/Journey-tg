2b1069ec0f13b1834501e5eb4fd4ef2cea7860a3
tgi-wtag <timo.giger@welldev.io>
2023-09-11 14:26:14 +0200

    Update journal_entries_controller.rb

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -1,33 +1,31 @@
 class JournalEntriesController < ApplicationController
     
-    def index
-        @journal_entries = JournalEntry.all
-    end
+  def index
+    @journal_entries = JournalEntry.all
+  end
   
-    def new
-      @journal_entry = JournalEntry.new
-    end
+  def new
+    @journal_entry = JournalEntry.new
+  end
   
-    def create
-      @journal_entry = JournalEntry.new(entry_params)
+  def create
+    @journal_entry = JournalEntry.new(entry_params)
   
-      if @journal_entry.save
-        if @journal_entry.title.present?
-          redirect_to journal_entry_path(@journal_entry)
-        else
-          flash[:error] = t('errors.no_title')
-        end
+    if @journal_entry.save
+      if @journal_entry.title.present?
+        redirect_to journal_entry_path(@journal_entry)
       else
-        flash[:error] = t('errors.creating_failed')
-        render :new
+        flash[:error] = t('errors.no_title')
       end
-
-
-    end
-
-    private
-    
-    def entry_params
-      params.require(:journal_entry).permit(:title, :date, :content)
+    else
+      flash[:error] = t('errors.creating_failed')
+      render :new
     end
   end
+
+  private
+    
+  def entry_params
+    params.require(:journal_entry).permit(:title, :date, :content)
+  end
+end
