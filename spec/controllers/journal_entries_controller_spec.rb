9bbdb2eb86a4afef7560c122d7b22589994ff0dc
tgi-wtag <timo.giger@welldev.io>
2023-09-15 11:07:49 +0200

    .

--- spec/controllers/journal_entries_controller_spec.rb
+++ spec/controllers/journal_entries_controller_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe JournalEntriesController, type: :controller do
-  let!(:journal_entry1) { FactoryBot.create(:journal_entry) }
+  # let(:journal_entry1) { FactoryBot.build(:journal_entry) } 
+  # @journal_entry1 = build(:journal_entry)
+  # @journal_entry2 = build(:journal_entry)
+  entry1 = JournalEntry.new
+  entry2 = JournalEntry.new
   describe 'GET #index' do
     it 'returns a successful response' do
       get :index
-      expect(response).to be_successful
+      expect(response).to render_template :index
     end
 
     it 'assigns @journal_entries' do
-      let!(:journal_entry2) { FactoryBot.create(:journal_entry) }
+      # let(:journal_entry2) { FactoryBot.build(:journal_entry) }
+      
       get :index
       expect(assigns(:journal_entries)).to eq([journal_entry1, journal_entry2])
     end
