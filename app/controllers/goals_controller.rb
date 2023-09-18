efe3172c6f0cbe1f324a09092ad334e2da2edecd
tgi-wtag <timo.giger@welldev.io>
2023-09-18 13:49:54 +0200

    .

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class GoalsController < ApplicationController
   def goals
     @goals = Goal.all
--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class JournalEntriesController < ApplicationController
   def index
     @journal_entries = JournalEntry.all
