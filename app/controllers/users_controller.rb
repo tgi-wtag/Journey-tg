a5afaeba8f8e477f8dc3e27022cf226590c644f2
tgi-wtag <timo.giger@welldev.io>
2023-09-20 14:33:00 +0200

    create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:fist_name, :last_name, :date_of_birth, :joining_date)
+  end
 end
