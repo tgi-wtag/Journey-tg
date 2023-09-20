98d9baf4d3d768b5cf13b2ae9f65e6a97e20765e
tgi-wtag <timo.giger@welldev.io>
2023-09-20 14:45:45 +0200

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new 
+    end
   end
 
   private
