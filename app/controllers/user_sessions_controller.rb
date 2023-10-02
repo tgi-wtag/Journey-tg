866b020f16bd84cf52a86c04a4f6401c80182613
tgi-wtag <timo.giger@welldev.io>
2023-10-02 11:54:04 +0200

    Update user_sessions_controller.rb

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,7 +1,17 @@
 class UserSessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by(first_name: params[:user][:first_name])
+
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:alert] = "Login failed"
+      redirect_to new_user_session_path
+    end
   end
 end
