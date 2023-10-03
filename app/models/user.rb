264a210e123900556c6fdb620582b9a93299cb9f
tgi-wtag <timo.giger@welldev.io>
2023-10-03 07:56:42 +0200

    errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :first_name, presence: true, uniqueness: true
   has_many :journal_entries
+  validates :first_name, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :date_of_birth, presence: true
+  validates :password, presence: true
+  validates_confirmation_of :password
 end
