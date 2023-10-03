5dc567bda285c71094cf103d0eca756c5b692263
tgi-wtag <timo.giger@welldev.io>
2023-10-03 11:40:26 +0200

    Create user_sessions_controller_spec.rb

---
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe UserSessionsController, type: :controller do
+  describe "GET #new" do
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "POST #create" do
+    let(:valid_attributes) { { email: "user@example.com", password: "password" } }
+    let(:invalid_attributes) { { email: "invalid@example.com", password: "invalid" } }
+    let!(:user) { User.create(valid_attributes) }
+
+    context "with valid credentials" do
+      it "creates a session and redirects to root path" do
+        post :create, params: { user: valid_attributes }
+        expect(session[:user_id]).to eq(user.id)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "with invalid credentials" do
+      it "sets flash alert and redirects to new user session path" do
+        post :create, params: { user: invalid_attributes }
+        expect(session[:user_id]).to be_nil
+        expect(flash[:alert]).to eq("Login failed")
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "clears the session and redirects to root path" do
+      session[:user_id] = 123
+      delete :destroy
+      expect(session[:user_id]).to be_nil
+      expect(response).to redirect_to(root_path)
+    end
+  end
+end
