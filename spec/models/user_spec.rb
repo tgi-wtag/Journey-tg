1347f4237570d2bc08437864c559b1877f5d6b75
tgi-wtag <timo.giger@welldev.io>
2023-10-04 11:06:34 +0200

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
-  describe "associations" do
+  describe 'associations' do
     it { should have_many(:journal_entries) }
   end
 
-  describe "validations" do
+  describe 'validations' do
     subject { FactoryBot.build(:user) }
 
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:email) }
-    it { should allow_value("user@example.com").for(:email) }
-    it { should_not allow_value("invalid_email").for(:email) }
+    it { should allow_value('user@example.com').for(:email) }
+    it { should_not allow_value('invalid_email').for(:email) }
     it { should validate_presence_of(:date_of_birth) }
     it { should validate_presence_of(:password) }
     it { should validate_confirmation_of(:password) }
