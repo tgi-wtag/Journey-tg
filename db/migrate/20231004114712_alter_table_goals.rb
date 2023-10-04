0ba835df5b5d42675487196fc5bd273dc9ec66a9
tgi-wtag <timo.giger@welldev.io>
2023-10-04 16:08:25 +0200

    Revert "goals"
    This reverts commit dcda31bc65e5556f5ac57250c6241d61918d3312.

--- db/migrate/20231004114712_alter_table_goals.rb
+++
@@ -1,9 +0,0 @@
-class AlterTableGoals < ActiveRecord::Migration[7.0]
-  def up
-    add_reference :goals, :user, foreign_key: true
-  end
-
-  def down
-    remove_reference :goals, :user, foreign_key: true
-  end
-end
