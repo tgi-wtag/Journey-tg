c0b681ebc1fe56354d478091cedca8cd9e87cd4a
tgi-wtag <timo.giger@welldev.io>
2023-10-04 09:34:50 +0200

    flash alerts

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -9,13 +9,14 @@ class UserSessionsController < ApplicationController
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to root_path
+      flash[:alert] = "Login fiugailed"
     else
       flash[:alert] = "Login failed"
       redirect_to new_user_session_path
     end
   end
   def destroy
-    session[:user_id] = nil
-    redirect_to root_path
-  end
+  session[:user_id] = nil
+  redirect_to root_path
+end
 end
