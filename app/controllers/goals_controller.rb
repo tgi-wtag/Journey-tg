e857b136092a447841ae31489d96ac7dfcf29394
tgi-wtag <timo.giger@welldev.io>
2023-10-04 15:04:10 +0200

    create new usw

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -2,4 +2,25 @@ class GoalsController < ApplicationController
   def goals
     @goals = Goal.all
   end
+
+
+  def new
+    @goal = Goal.new
+  end
+  def create
+    @goal= Goal.new(entry_params)
+    @goal.user_id = current_user.id
+
+    if @goal.save
+      redirect_to goals_path(@goals)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def entry_params
+    params.require(:goal).permit(:description, :deadline)
+  end
 end
