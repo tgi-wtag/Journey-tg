8953f2e5ffe8290f6e5a7d1593f02fc7274f735b
tgi-wtag <timo.giger@welldev.io>
2023-10-05 15:45:42 +0200

    Update user_sessions_spec.rb

--- spec/requests/user_sessions_spec.rb
+++ spec/requests/user_sessions_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/user_sessions/new"
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/user_sessions/new'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/user_sessions/create"
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/user_sessions/create'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
