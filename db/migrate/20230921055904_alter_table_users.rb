aa1bc0c19728f56a2fe91f76a2197ddae57dd556
tgi-wtag <timo.giger@welldev.io>
2023-10-09 11:51:10 +0200

    changes 1

--- db/migrate/20230921055904_alter_table_users.rb
+++ db/migrate/20230921055904_alter_table_users.rb
@@ -2,10 +2,11 @@ class AlterTableUsers < ActiveRecord::Migration[7.0]
   def up
     add_column :users, :role, :integer, default: 0, null: false
     add_column :users, :designation, :string
-    add_column :users, :email, :string
-    add_column :users, :password_digest, :string
-    add_column :users, :password_confirmation, :string
+    add_column :users, :email, :string, null: false
+    add_column :users, :password_digest, :string, null: false
+    add_column :users, :password_confirmation, :string, null: false
     rename_column :users, :fist_name, :first_name
+    update_colums :users, :date_of_birth, , null: false
   end
 
   def down
