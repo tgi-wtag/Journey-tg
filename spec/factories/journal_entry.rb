4e31793d88a18b1b459551f318172990e7a691ad
Timo Giger <141820846+tgi-wtag@users.noreply.github.com>
2023-10-11 12:05:47 +0200

    Update spec/factories/journal_entry.rb
    Co-authored-by: Evea Zerin Khan <90237465+evk-wtag@users.noreply.github.com>

--- spec/factories/journal_entry.rb
+++ spec/factories/journal_entry.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :journal_entry do
+    user
     title { Faker::Lorem.sentence }
     date { Faker::Date.backward(days: 365) }
     content { Faker::Lorem.paragraph }
