7d0917e2191babd560b62b23a58a3e0c578a79a6
tgi-wtag <timo.giger@welldev.io>
2023-10-11 11:26:38 +0200

    association and routes syntax

--- spec/models/journal_entry_spec.rb
+++ spec/models/journal_entry_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe JournalEntry, type: :model do
+  it { should belong_to(:user) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:content) }
 end
