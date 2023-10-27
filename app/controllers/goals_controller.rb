0805af908bf22a08928e4a83507068eed0552e65
tgi-wtag <timo.giger@welldev.io>
2023-10-27 10:50:34 +0200

    updated routes, enbeded goal in user

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,38 +1,28 @@
 class GoalsController < ApplicationController
-  before_action :set_goal, only: %i[show edit update]
+  before_action :load_goal, only: %i[show edit update] 
 
   def goals
     @goals = Goal.all
   end
 
   def new
-    @goal = Goal.new
+    @goal = current_user.goals.new
   end
 
   def create
-    @goal = Goal.new(goal_params)
-    @goal.user_id = current_user.id
+    @goal = current_user.goals.new(goal_params)
 
     if @goal.save
-      if @goal.description.present?
-        redirect_to goal_path(@goal)
-      else
-        flash[:error] = t('errors.no_description')
-        redirect_to new_goal_path
-      end
+      redirect_to user_goals_path
     else
       flash[:error] = t('errors.creating_failed')
       render :new
     end
   end
 
-  def show; end
-
-  def edit; end
-
   def update
     if @goal.update(goal_params)
-      redirect_to goal_path(@goal)
+      redirect_to user_goal_path(@user, @goal)
     else
       render :edit
     end
@@ -44,7 +34,8 @@ class GoalsController < ApplicationController
     params.require(:goal).permit(:description, :deadline, :status)
   end
 
-  def set_goal
-    @goal = Goal.find(params[:id])
+  def load_goal
+    @user = User.find(params[:user_id])
+    @goal = @user.goals.find(params[:id])
   end
 end
