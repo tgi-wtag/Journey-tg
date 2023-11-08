574289daa7757d7e41e36a51c928603fb8255851
tgi-wtag <timo.giger@welldev.io>
2023-11-08 15:54:56 +0100

    specs

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,11 @@ class Ability
   def initialize(user)
     if user.admin?
       can manage:, :all
-    else
+    elsif user.super_admin?
       can manage:, :all
+    else 
+      can :manage, :all
+      cannot :update_role, User
     end
   end
 end
