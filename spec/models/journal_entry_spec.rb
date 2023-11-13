3c926a6dc48d89f7af0887b0be0df97e6105b3e6
tgi-wtag <timo.giger@welldev.io>
2023-11-13 10:19:31 +0100

    .

--- spec/models/journal_entry_spec.rb
+++ spec/models/journal_entry_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe JournalEntry, type: :model do
-
   it { should belong_to(:user) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:content) }
